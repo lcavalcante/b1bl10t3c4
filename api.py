7a9c96ce6a4242b4f008b88928f6f7beff30ca98
lcavalcante <lucasmcavalcante@gmail.com>
2016-04-19 17:35:51 -0300

    REST API

--- api.py
+++ api.py
@@ -1,10 +1,62 @@
 import webapp2
+import json
+
+import model
 
 
-class BookHandler(webapp2.RequestHandler):
+def as_dict(book):
+    return {'id': book.key.id(),
+            'title': book.title,
+            'author': book.author,
+            'price': book.price,
+            'desc': book.desc,
+            'photo': book.photo}
+
+
+class APIHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        super(APIHandler, self).dispatch()
+
+    def send_json(self, r):
+        # self.response.headers['content-type'] = 'text/plain'
+        self.response.write(json.dumps(r))
+
+
+class QueryHandler(APIHandler):
     def get(self):
-        self.response.write('Hello world!')
+        books = model.all_books()
+        r = [as_dict(book) for book in books]
+        self.send_json(r)
+
+
+class UpdateHandler(APIHandler):
+    def put(self, id):
+        r = json.loads(self.request.body)
+        book = model.update_book(id, r['title'], r['author'], r['price'], r['desc'], r['photo'])
+        r = as_dict(book)
+        self.send_json(r)
+
+
+class InsertHandler(APIHandler):
+    def post(self):
+        r = json.loads(self.request.body)
+        book = model.insert_book(r['title'], r['author'], r['price'], r['desc'], r['photo'])
+        r = as_dict(book)
+        self.send_json(r)
+
+
+class DeleteHandler(APIHandler):
+    def delete(self, key=None):
+        r = json.loads(self.request.body)
+        if key is not None:
+            model.delete_book(key)
+        else:
+            model.delete_book(r['id'])
+
 
 app = webapp2.WSGIApplication([
-    ('/api', BookHandler)
+    ('/api/query', QueryHandler),
+    ('/api/insert', InsertHandler),
+    ('/api/delete', DeleteHandler),
+    ('/api/update', UpdateHandler),
 ], debug=True)
---
+++ model.py
@@ -0,0 +1,31 @@
+from google.appengine.ext import ndb
+
+
+class Book(ndb.Model):
+
+        title = ndb.StringProperty()
+        author = ndb.StringProperty()
+        price = ndb.IntegerProperty()
+        desc = ndb.StringProperty()
+        photo = ndb.StringProperty()
+
+
+def all_books():
+    return Book.query()
+
+
+def insert_book(title, author, price, desc, photo):
+    book = Book(title=title, author=author, price=price, desc=desc, photo=photo)
+    book.put()
+    return book
+
+
+def update_book(id, title, author, price, desc, photo):
+    book = Book(id=id, title=title, author=author, price=price, desc=desc, photo=photo)
+    book.put()
+    return book
+
+
+def delete_book(number):
+    key = ndb.Key(Book, number)
+    key.delete()
