d3603d4a382274ea5cf5c6d6b1058679fcc7a851
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2016-05-02 19:17:12 -0300

    Front end terminado

--- js/db.js
+++ js/db.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var Desc = function (autor, titulo, texto) {
+var Comment = function (autor, titulo, texto) {
     var self = this;
 
     self.titulo = titulo;
@@ -32,7 +32,8 @@ var Livro = function (titulo, autores, desc, foto, preco) {
     this.setDesc = function (desc) {
         self.desc = desc;
     };
-    this.addComment = function (comment) {
+    this.addComment = function (autor, titulo, texto) {
+        var comment = new Comment(autor, titulo, texto);
         self.comments.push(comment);
     }
 
@@ -40,18 +41,15 @@ var Livro = function (titulo, autores, desc, foto, preco) {
 
 
 var l1 = new Livro("The Lord of the Rings", "J.R.R Tolkien", "A história narra o conflito contra o mal que se alastra pela Terra-média, através da luta de várias raças - Humanos, Anões, Elfos, Ents e Hobbits - contra Orcs, para evitar que o Anel do Poder volte às mãos de seu criador Sauron, o Senhor do Escuro. Partindo dos primórdios tranquilos do Condado, a história muda através da Terra-média e segue o curso da Guerra do Anel através dos olhos de seus personagens, especialmente do protagonista, Frodo Bolseiro.", "img/lotr2.jpg", 20);
-var l2 = new Livro("The Hunger Games", "Suzanne Collins", "1", "img/hg.jpg", 20);
-var l3 = new Livro("The Chronicles of Narnia", "C. S. Lewis", "sa", "img/cn.jpg", 30);
-var l4 = new Livro("The Great Gatsby", "F. Scott Fitzgerald", "a", "img/gg.jpg", 40);
-var l5 = new Livro("Planet of the Apes", "Pierre Boulle", "a", "img/pa.jpg", 50);
-var l6 = new Livro("Ensaio Sobre a Cegueira", "Jose Saramago", "a", "img/ec.jpg", 60);
-var l7 = new Livro("The Godfather", "Mario Puzo", "a", "img/tg.jpg", 70);
-var l8 = new Livro("I Robot", "Isaac Asimov", "a", "img/ir.jpg", 80);
-var l9 = new Livro("War And Peace", "Leo Tolstoy", "a", "img/wp.jpg", 90);
-var l10 = new Livro("Les Miserables", "Victor Hugo", "a", "img/lm.jpg", 100);
+var l2 = new Livro("The Hunger Games", "Suzanne Collins", "Este livro é o primeiro de uma bem-sucedida trilogia, comercializada para mais de 20 países, A história se passa em uma nação chamada Panem, fundada após o fim da América do Norte. Formada por 12 distritos, é comandada com mão de ferro pela Capital, sede do governo. Uma das formas com que demonstra seu poder sobre o resto do carente país é com os 'Jogos Vorazes', uma competição anual transmitida ao vivo pela televisão, em que um garoto e uma garota de 12 a 18 anos de cada distrito são selecionados e obrigados a lutar até a morte. ", "img/hg.jpg", 20);
+var l3 = new Livro("The Chronicles of Narnia", "C. S. Lewis", "Nos últimos 50 anos, 'As Crônicas de Nárnia' transcenderam o gênero da fantasia para se tornar parte do cânone da literatura clássica. Cada um dos sete livros é uma obra-prima, atraindo o leitor para um mundo em que a magia encontra a realidade, e o resultado é um mundo ficcional que tem fascinado gerações. Esta edição apresenta todas as sete crônicas integralmente, um único volume magnífico. Os livros são apresentados de acordo com a ordem de preferência de Lewis, cada capítulo com um ilustração do artista original, Pauline Baynes. Enganosamente simples e diretas, 'As Crônicas de Nárnia' continuam cativando os leitores com aventuras, personagens e fatos que falam a pessoas de todas as idades, mesmo 50 anos após terem sido publicadas pela primeira vez.", "img/cn.jpg", 30);
+var l4 = new Livro("The Great Gatsby", "F. Scott Fitzgerald", "Jay Gatsby is the man who has everything. But one thing will always be out of his reach. Everybody who is anybody is seen at his glittering parties. Day and night his Long Island mansion buzzes with bright young things drinking, dancing and debating his mysterious character.", "img/gg.jpg", 40);
+var l5 = new Livro("Planet of the Apes", "Pierre Boulle", "Em pouco tempo, os desbravadores do espaço descobrem a terrível verdade: nesse mundo, seus pares humanos não passam de bestas selvagens a serviço da espécie dominante... os macacos. Desde as primeiras páginas até o surpreendente ?nal – ainda mais impactante que a famosa cena ?nal do ?lme de 1968 –, O planeta dos macacos é um romance de tirar o fôlego, temperado com boa dose de sátira. Nele, Boulle revisita algumas das questões mais antigas da humanidade: O que de?ne o homem? O que nos diferencia dos animais? Quem são os verdadeiros inimigos de nossa espécie? Publicado pela primeira vez em 1963, O planeta dos macacos, de Pierre Boulle, inspirou uma das mais bem-sucedidas franquias da história do cinema, tendo início no clássico de 1968, estrelado por Charlton Heston, passando por diversas sequências e chegando às adaptações cinematográ?cas mais recentes. Com milhões de exemplares vendidos ao redor do mundo, O planeta dos macacos é um dos maiores clássicos da ?cção cientí?ca, imprescindível aos fãs de cultura pop.", "img/pa.jpg", 50);
+var l6 = new Livro("Ensaio Sobre a Cegueira", "Jose Saramago", "Livro que deu origem ao filme de Fernando Meirelles. Do autor vencedor do Prêmio Nobel de Literatura de 1998. Um motorista parado no sinal se descobre subitamente cego. É o primeiro caso de uma 'treva branca' que logo se espalha incontrolavelmente. Resguardados em quarentena, os cegos se perceberão reduzidos à essência humana, numa verdadeira viagem às trevas.O Ensaio sobre a cegueira é a fantasia de um autor que nos faz lembrar 'a responsabilidade de ter olhos quando os outros os perderam'. José Saramago nos dá, aqui, uma imagem aterradora e comovente de tempos sombrios, à beira de um novo milênio, impondo-se à companhia dos maiores visionários modernos, como Franz Kafka e Elias Canetti. Cada leitor viverá uma experiência imaginativa única. Num ponto onde se cruzam literatura e sabedoria, José Saramago nos obriga a parar, fechar os olhos e ver. Recuperar a lucidez, resgatar o afeto: essas são as tarefas do escritor e de cada leitor, diante da pressão dos tempos e do que se perdeu: 'uma coisa que não tem nome, essa coisa é o que somos'.", "img/ec.jpg", 60);
+var l7 = new Livro("The Godfather", "Mario Puzo", "A fictional portrait journeys inside the world of the Cosa Nostra and its operations to chronicle the lives and fortunes of Mafia leader Vito Corleone, his family, and his underworld domain. Reissue.", "img/tg.jpg", 70);
+var l8 = new Livro("I Robot", "Isaac Asimov", "In the late 1940s and early 1950s Isaac Asimov found a home on the pages of the science-fiction magazines Astounding and Super-Science Stories. World War II had just ended and the world was obsessed with air combat and the role of technology in society. Asimov’s stories reflected the concerns over the danger of technology but they also humanized robots, indicating that it is not technology that is evil but the way it is sometimes abused.", "img/ir.jpg", 80);
+var l9 = new Livro("War And Peace", "Leo Tolstoy", "Com um número quase sem fim de personagens e mais de mil páginas em sua versão original, Guerra e paz é um dos mais significativos e importantes romances da história. O enredo deste clássico da literatura russa (e mundial) se desenrola durante a campanha de Napoleão Bonaparte na Áustria, e descreve a invasão da Rússia pelo exército francês e a sua retirada, entre os anos de 1805 a 1820. O autor pinta com habilidade e desenvoltura um retrato acurado do jogo da política da época, das intrigas vigentes na corte, das tramas da sociedade, das manobras propostas pela nobreza arruinada, da brutalidade da guerra, sua banalidade e pormenores.", "img/wp.jpg", 90);
+var l10 = new Livro("Les Miserables", "Victor Hugo", "In this story of the trials of the peasant Jean Valjean - a man unjustly imprisoned, baffled by destiny, and hounded by his nemesis, the magnificently realized, ambiguously malevolent police detective Javert - Hugo achieves the sort of rare imaginative resonance that allows a work of art to transcend its genre.Trying to forget his past and live an honest life, escaped convict Jean Valjean risks his freedom to take care of a motherless young girl during a period of political unrest in Paris.", "img/lm.jpg", 100);
 
-var c1 = new Desc("Victor Victorius Viton", "O Melhor", "Eu amo frodo bolseiro");
-var c2 = new Desc("Leticia, A Cabeluda", "Legolas", "Orlando Bloom muito gatinho");
-
-l1.addComment(c1);
-l1.addComment(c2);
+l1.addComment("Victor Victorius Viton", "O Melhor", "Eu amo frodo bolseiro");
+l1.addComment("Leticia, A Cabeluda", "Legolas", "Orlando Bloom muito gatinho");
--- js/lib.js
+++ js/lib.js
@@ -2,68 +2,76 @@
 
 var victor = null;
 angular.module("lib", [])
-    .controller('libController', function (){
-    var self = this;
-    victor = self;
-    self.livros = [];
-    self.livros.push(l1);
-    self.livros.push(l2);
-    self.livros.push(l3);
-    self.livros.push(l4);
-    self.livros.push(l5);
-    self.livros.push(l6);
-    self.livros.push(l7);
-    self.livros.push(l8);
-    self.livros.push(l9);
-    self.livros.push(l10);
+    .controller('libController', function () {
+        var self = this;
+        victor = self;
+        self.livros = [];
+        self.livros.push(l1);
+        self.livros.push(l2);
+        self.livros.push(l3);
+        self.livros.push(l4);
+        self.livros.push(l5);
+        self.livros.push(l6);
+        self.livros.push(l7);
+        self.livros.push(l8);
+        self.livros.push(l9);
+        self.livros.push(l10);
 
-    self.indiceLivro = 0;
-    
-    self.selected = null;
+        self.indiceLivro = 0;
 
-    self.livro = self.livros[self.indiceLivro];
+        self.selected = null;
 
-
-    self.addLivro = function(titulo, autores, desc, foto, preco){
-        var novoLivro = new Livro(titulo, autores, desc, foto, preco);
-        self.livros.push(novoLivro);
-    };
-
-    self.updateLivro = function(indice, titulo, autores, desc, foto, preco){
-        self.livros[indice].setTitulo(titulo);
-        self.livros[indice].setAutores(autores);
-        self.livros[indice].setDesc(desc);
-        self.livros[indice].setFoto(foto);
-        self.livros[indice].setPreco(preco);
-    };
-    
-    self.deleteLivro = function(index){
-        if (index > -1){
-            self.livros.splice(index,1);
-        }
-    };
-
-    self.addComment = function(comment){
-        self.livro.setComment(comment);
-    };
-    
-    self.select = function(book){
-        self.selected = book;
-    };
-
-    self.next = function(){
-        self.indiceLivro += 1;
-        if(self.indiceLivro >= self.livros.length){
-            self.indiceLivro = 0;
-        }
         self.livro = self.livros[self.indiceLivro];
-    };
-    self.prev = function(){
-        self.indiceLivro -= 1;
-        if(self.indiceLivro < 0){
-            self.indiceLivro = self.livros.length-1;
-        }
-        self.livro = self.livros[self.indiceLivro];
-    }
 
-});
+
+        self.addLivro = function (titulo, autores, desc, foto, preco) {
+            var novoLivro = new Livro(titulo, autores, desc, foto, preco);
+            self.livros.push(novoLivro);
+        };
+
+        self.updateLivro = function (indice, titulo, autores, desc, foto, preco) {
+            self.livros[indice].setTitulo(titulo);
+            self.livros[indice].setAutores(autores);
+            self.livros[indice].setDesc(desc);
+            self.livros[indice].setFoto(foto);
+            self.livros[indice].setPreco(preco);
+        };
+
+        self.deleteLivro = function (index) {
+            if (index > -1) {
+                self.livros.splice(index, 1);
+            }
+        };
+
+        self.findLivro = function (title) {
+            for (var i = 0; i < self.livros.length; i++){
+                if(self.livros[i].titulo === title){
+                    return i;
+                }
+            }
+        };
+
+        self.addComment = function (comment) {
+            self.livro.setComment(comment);
+        };
+
+        self.select = function (book) {
+            self.selected = book;
+        };
+
+        self.next = function () {
+            self.indiceLivro += 1;
+            if (self.indiceLivro >= self.livros.length) {
+                self.indiceLivro = 0;
+            }
+            self.livro = self.livros[self.indiceLivro];
+        };
+        self.prev = function () {
+            self.indiceLivro -= 1;
+            if (self.indiceLivro < 0) {
+                self.indiceLivro = self.livros.length - 1;
+            }
+            self.livro = self.livros[self.indiceLivro];
+        }
+
+    });
